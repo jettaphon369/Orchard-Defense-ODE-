e9331836fe7266b69d23eba6134e4192707e707e
jettaphon369 <jettaphon369@gmail.com>
2026-07-13 08:46:24 +0700

    Delete Plant.js

--- Plant.js
+++
@@ -1,8 +0,0 @@
-export class Plant {
-  constructor({ def, row, col, mesh }) {
-    Object.assign(this, { def, row, col, mesh });
-    this.hp = def.hp;
-    this.cooldown = 0;
-    this.productionTimer = 0;
-  }
-}
